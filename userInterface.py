9263995d51cbfde034db0f4f234e14641cf964db
cortisiko <curtis.david7@gmail.com>
2020-04-20 20:03:20 -0400

    adding the clear method functionality

--- userInterface.py
+++ userInterface.py
@@ -1,4 +1,3 @@
-from tkinter import *
 import analyze as anlyze
 
 try:
@@ -12,36 +11,42 @@ class UserInterface():
         self.root = tk.Tk()
         self.windowTitle = self.root.title("Stock Analyzer")
         self.root.geometry('600x240')
-        self.label = tk.Label(self.root, text="Stock Symbol: ")
-        self.label.pack(side="top")
-        self.textInputBox = tk.Text(self.root, relief=RIDGE, height=1, width = 6, borderwidth=2)
+        self.stockSymbolText = tk.Label(self.root, text="Stock Symbol: ")
+        self.stockSymbolText.pack(side="top")
+        self.textInputBox = tk.Text(self.root, relief=tk.RIDGE, height=1, width = 6, borderwidth=2)
         self.analyzeButton = tk.Button(self.root,
-                                text='Analyze',relief=RIDGE,
+                                text='Analyze Stock',relief=tk.RIDGE,
                                 command=lambda: [self.getEPS(), self.getPERatio(), self.getReturnOnEquity(),
                                                  self.currentStockPrice(), self.getDebtToEquityRatio(),
                                                  self.getProfitMargin()])
 
         #self.label = tk.Label(self.root, text="This will be cleared.")
-        self.clearButton = tk.Button(self.root, text="Clear",
-                           command=lambda: self.clear_widget())
+        self.clearButton = tk.Button(self.root, text="Clear", command=self.clearValues)
 
         self.textInputBox.pack()
         self.analyzeButton.pack()
         self.clearButton.pack()
 
-        self.labelA = tk.Label(self.root, text="Earnings Per Share:")
-        self.labelB = tk.Label(self.root, text="PE Ratio:")
-        self.labelC = tk.Label(self.root, text="Return on Equity Ratio:")
-        self.labelD = tk.Label(self.root, text="Current Stock Price:")
-        self.labelE = tk.Label(self.root, text="Debt to Equity Ratio:")
-        self.labelF = tk.Label(self.root, text="Profit Margin")
+        self.earningsPerShareLabelText = tk.Label(self.root, text="Earnings Per Share:")
+        self.peRatioLabelText = tk.Label(self.root, text="PE Ratio:")
+        self.returnOnEquityLabelText = tk.Label(self.root, text="Return on Equity Ratio:")
+        self.currentStockPriceLabelText = tk.Label(self.root, text="Current Stock Price:")
+        self.debtToEquityRatioLabelText = tk.Label(self.root, text="Debt to Equity Ratio:")
+        self.profitMarginLabelText = tk.Label(self.root, text="Profit Margin")
 
-        self.labelA.pack()
-        self.labelB.pack()
-        self.labelC.pack()
-        self.labelD.pack()
-        self.labelE.pack()
-        self.labelF.pack()
+        self.earningsPerShareLabelDefault = "Earnings Per Share:"
+        self.peRatioLabelDefault = "PE Ratio:"
+        self.returnOnEquityLabelDefault = "Return on Equity Ratio:"
+        self.currentStockPriceLabelDefault = "Current Stock Price:"
+        self.debtToEquityRatioLabelDefault = "Debt to Equity Ratio:"
+        self.profitMarginLabelDefault = "Profit Margin:"
+
+        self.earningsPerShareLabelText.pack()
+        self.peRatioLabelText.pack()
+        self.returnOnEquityLabelText.pack()
+        self.currentStockPriceLabelText.pack()
+        self.debtToEquityRatioLabelText.pack()
+        self.profitMarginLabelText.pack()
         self.root.iconbitmap("stockx.ico")
         self.root.mainloop()
 
@@ -49,33 +54,33 @@ class UserInterface():
         tickerFromUser = self.getTextInput()
         eps = anlyze.getEPS(tickerFromUser)
 
-        self.labelA["text"] = self.labelA["text"] + str(eps)
+        self.earningsPerShareLabelText["text"] = self.earningsPerShareLabelText["text"] + str(eps)
 
     def getPERatio(self):
         tickerFromUser = self.getTextInput()
         peRatio = anlyze.getPERatio(tickerFromUser)
 
-        self.labelB["text"] = self.labelB["text"] + str(peRatio)
+        self.peRatioLabelText["text"] = self.peRatioLabelText["text"] + str(peRatio)
 
     def getReturnOnEquity(self):
         tickerFromUser = self.getTextInput()
         returnOnEquity = anlyze.getReturnOnEquity(tickerFromUser)
-        self.labelC["text"] = self.labelC["text"] + str(returnOnEquity)
+        self.returnOnEquityLabelText["text"] = self.returnOnEquityLabelText["text"] + str(returnOnEquity)
 
     def currentStockPrice(self):
         tickerFromUser = self.getTextInput()
         peRatio = anlyze.getCurrentStockPrice(tickerFromUser)
-        self.labelD["text"] = self.labelD["text"] + str(peRatio)
+        self.currentStockPriceLabelText["text"] = self.currentStockPriceLabelText["text"] + str(peRatio)
 
     def getDebtToEquityRatio(self):
         tickerFromUser = self.getTextInput()
         debtToEquityRatio = anlyze.getDebtToEquity(tickerFromUser)
-        self.labelE["text"] = self.labelE["text"] + str(debtToEquityRatio)
+        self.debtToEquityRatioLabelText["text"] = self.debtToEquityRatioLabelText["text"] + str(debtToEquityRatio)
 
     def getProfitMargin(self):
         tickerFromUser = self.getTextInput()
         profitMargin = anlyze.getProfitMargin(tickerFromUser)
-        self.labelF["text"] = self.labelF["text"] + str(profitMargin)
+        self.profitMarginLabelText["text"] = self.profitMarginLabelText["text"] + str(profitMargin)
 
     def getTextInput(self):
         result = self.textInputBox.get("1.0", "end")
@@ -87,8 +92,13 @@ class UserInterface():
         userInput = self.getTextInput()
         anlyze.analyzeStock(userInput)
 
-    def clear_widget(widget):
-        widget.destroy()
-
+    def clearValues(self):
+        self.earningsPerShareLabelText["text"]  = self.earningsPerShareLabelDefault
+        self.peRatioLabelText["text"]           = self.peRatioLabelDefault
+        self.returnOnEquityLabelText["text"]    = self.returnOnEquityLabelDefault
+        self.currentStockPriceLabelText["text"] = self.currentStockPriceLabelDefault
+        self.debtToEquityRatioLabelText["text"] = self.debtToEquityRatioLabelDefault
+        self.profitMarginLabelText["text"]      = self.profitMarginLabelDefault
+        self.textInputBox.delete("1.0", "end")
 
 app = UserInterface()
