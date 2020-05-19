1261ff19ba508f273daf5b65300a83e93f314f17
cortisiko <curtis.david7@gmail.com>
2020-05-19 13:32:43 -0400

    add bar graph to net income page

--- charts/plotNetIncome.py
+++ charts/plotNetIncome.py
@@ -2,6 +2,8 @@ import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
+
 from Financials import price as priceData
 from Financials import incomeStatementSheet as income
 
@@ -30,7 +32,15 @@ class PlotGraph:
         ax.set_ylabel(yLabelText)
 
         ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
-        ax.plot(dates, incomeStatement, '-o', color='green')
+
+        ax.bar(dates, incomeStatement, color=['r' if v < 0 else 'g' for v in incomeStatement])
+        for i, v in enumerate(incomeStatement):
+            ax.text(i, v * 0.75, f'{v:,.0f}', fontweight='bold', va='center', ha='center',color ='#0A0A0A')
+
+        legend_handles = [
+            Line2D([0], [0], linewidth=0, marker='o', markerfacecolor=color, markersize=12, markeredgecolor='none')
+            for color in ['g', 'r']]
+        ax.legend(legend_handles, ['positive net income', 'negative net income'])
 
         if not self.canvas:
             self.canvas = FigureCanvasTkAgg(self.fig, container)
