2ca2d3470664b4adecf93f41ea0a9bace64fa51d
cortisiko <curtis.david7@gmail.com>
2020-05-20 19:30:02 -0400

    remove colors in debt chart

--- charts/plotLongTermDebt.py
+++ charts/plotLongTermDebt.py
@@ -32,7 +32,7 @@ class PlotGraph:
 
         ax.get_yaxis().set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, p: format(int(x), ',')))
 
-        ax.bar(dates, longTermDebt, color=['r' if v < 0 else 'g' for v in longTermDebt])
+        ax.bar(dates, longTermDebt)
         for i, v in enumerate(longTermDebt):
             ax.text(i, v * 0.75, f'${v:,.0f}', fontweight='bold', va='center', ha='center')
 
