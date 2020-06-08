e3442a80bd33748796ab59d015fb9423f939112a
cortisiko <curtis.david7@gmail.com>
2020-06-08 19:46:52 -0400

    adding a page for the netincome

---
+++ pages/incomeStatementPage.py
@@ -0,0 +1,71 @@
+try:
+    import Tkinter as tk
+except:
+    import tkinter as tk
+import tkinter.font
+from tkinter import *
+
+from pages import homePage
+from charts import plotNetIncome as pltIncome
+from helpers import getMessageBox as messagebox
+
+invalidTickerErrorMessage = "Sorry, you need to enter a ticker symbol"
+
+class IncomeStatement(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self['bg'] = '#1B6666'
+        self.controller = controller
+        self.my_font = tkinter.font.Font(self, family="Sans Serif", size=20)
+        self.pageTitle = Label(self, text="Net Income Charts", font=self.my_font)
+        self.RadioText = StringVar()
+        self.quarterlyTextString = 'q'
+        self.yearlyTextString = 'a'
+
+        self.textInputBox = Text(self, relief=tk.RIDGE, height=1, width=6, borderwidth=2)
+        self.textInputBox.focus()
+        self.frequencyText = Label(self, text="Frequency")
+        self.quarterlyRadioButton = Radiobutton(self, text="Quarterly", variable=self.RadioText,
+                                                value=self.quarterlyTextString, command=self.selectedRadioButtonOption)
+        self.yearlyRadioButton = Radiobutton(self, text="Annual", variable=self.RadioText, value=self.yearlyTextString,
+                                             command=self.selectedRadioButtonOption)
+        self.clearButton = Button(self, text='Clear', command=self.clear, bg='red')
+
+        self.pageTitle.pack()
+        self.textInputBox.pack()
+        self.quarterlyRadioButton.pack(side='left', padx=50)
+        self.yearlyRadioButton.pack(side='right', padx=50)
+        self.clearButton.pack()
+
+        button1 = Button(self, text="Back to Home",
+                         command=lambda: controller.show_frame(homePage.Startpage))
+
+    def getTextInput(self):
+        result = self.textInputBox.get("1.0", "end")
+        result = result.rstrip()
+        if len(result) > 0:
+            results = result.upper()
+            results = str(results)
+            return results
+        else:
+            self.yearlyRadioButton.deselect()
+            self.quarterlyRadioButton.deselect()
+            messagebox.showErrorMessage(self,invalidTickerErrorMessage)
+
+    def selectedRadioButtonOption(self):
+        userInput = self.getTextInput()
+        radioButtonFrequencyOption = self.RadioText.get()
+        if not income.canvas:
+            income.netIncomeChart(self, userInput, radioButtonFrequencyOption)
+
+        else:
+            income.clearPlotPage()
+            income.netIncomeChart(self, userInput, radioButtonFrequencyOption)
+
+    def clear(self):
+        self.textInputBox.delete("1.0", "end")
+        income.clearPlotPage()
+        self.yearlyRadioButton.deselect()
+        self.quarterlyRadioButton.deselect()
+
+income = pltIncome.PlotGraph()
--- userInterface.py
+++ userInterface.py
@@ -4,12 +4,9 @@ except:
     import tkinter as tk
 import tkinter.font
 from tkinter import *
-from PIL import ImageTk, Image as kkImage
-from pages import cashFlow
-from pages import homePage
+from pages import cashFlowPage,homePage,incomeStatementPage
 
 from charts import plotCashFlow as pltCashFlow, plotNetIncome as pltIncome, plotEarnings as pltEarnings,plotRevenue as pltRevenue,plotLongTermDebt as pltDebt, cashBasedEarningsChart as cashToEarnings
-from Financials import analyze as anlyze
 from helpers import getMessageBox as messagebox
 
 
@@ -38,17 +35,17 @@ class UserInterFace(tk.Tk):
         Charts = tk.Menu(menu, tearoff=0)
         menu.add_cascade(menu=Charts, label="Charts")
 
-        Charts.add_command(label="Cash Flow", command=lambda: self.show_frame(cashFlow.cashflow))
+        Charts.add_command(label="Cash Flow", command=lambda: self.show_frame(cashFlowPage.cashflow))
         Charts.add_command(label="Cash to Earnings", command=lambda: self.show_frame(plotCashToEarnings))
         Charts.add_command(label="Long Term Debt", command=lambda: self.show_frame(plotDebtGraph))
-        Charts.add_command(label="Net Income", command=lambda: self.show_frame(PlotIncomeStatementChart))
+        Charts.add_command(label="Net Income", command=lambda: self.show_frame(incomeStatementPage.IncomeStatement))
         Charts.add_command(label="Revenue", command=lambda: self.show_frame(plotRevenueChart))
 
 
         menu.add_separator()
         tk.Tk.config(self, menu=menu)
 
-        for F in (homePage.Startpage, cashFlow.cashflow,plotDebtGraph, PlotIncomeStatementChart,plotRevenueChart,plotCashToEarnings):
+        for F in (homePage.Startpage, cashFlowPage.cashflow, plotDebtGraph, incomeStatementPage.IncomeStatement, plotRevenueChart, plotCashToEarnings):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -64,63 +61,6 @@ class UserInterFace(tk.Tk):
 
 
 #   *****   PAGES   *****
-
-class PlotIncomeStatementChart(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self['bg'] = '#1B6666'
-        self.controller = controller
-        self.my_font = tkinter.font.Font(self, family="Sans Serif", size=20)
-        self.pageTitle = Label(self, text="Net Income Charts", font=self.my_font)
-        self.RadioText = StringVar()
-        self.quarterlyTextString = 'q'
-        self.yearlyTextString = 'a'
-
-        self.textInputBox = Text(self, relief=tk.RIDGE, height=1, width=6, borderwidth=2)
-        self.textInputBox.focus()
-        self.frequencyText = Label(self, text="Frequency")
-        self.quarterlyRadioButton = Radiobutton(self, text="Quarterly", variable=self.RadioText,
-                                                value=self.quarterlyTextString, command=self.selectedRadioButtonOption)
-        self.yearlyRadioButton = Radiobutton(self, text="Annual", variable=self.RadioText, value=self.yearlyTextString,
-                                             command=self.selectedRadioButtonOption)
-        self.clearButton = Button(self, text='Clear', command=self.clear, bg='red')
-
-        self.pageTitle.pack()
-        self.textInputBox.pack()
-        self.quarterlyRadioButton.pack(side='left', padx=50)
-        self.yearlyRadioButton.pack(side='right', padx=50)
-        self.clearButton.pack()
-
-        button1 = Button(self, text="Back to Home",
-                         command=lambda: controller.show_frame(homePage.Startpage))
-
-    def getTextInput(self):
-        result = self.textInputBox.get("1.0", "end")
-        result = result.rstrip()
-        if len(result) > 0:
-            results = result.upper()
-            results = str(results)
-            return results
-        else:
-            self.yearlyRadioButton.deselect()
-            self.quarterlyRadioButton.deselect()
-            messagebox.showErrorMessage(self,invalidTickerErrorMessage)
-
-    def selectedRadioButtonOption(self):
-        userInput = self.getTextInput()
-        radioButtonFrequencyOption = self.RadioText.get()
-        if not income.canvas:
-            income.netIncomeChart(self, userInput, radioButtonFrequencyOption)
-
-        else:
-            income.clearPlotPage()
-            income.netIncomeChart(self, userInput, radioButtonFrequencyOption)
-
-    def clear(self):
-        self.textInputBox.delete("1.0", "end")
-        income.clearPlotPage()
-        self.yearlyRadioButton.deselect()
-        self.quarterlyRadioButton.deselect()
 
 
 class plotEarningsChart(tk.Frame):
