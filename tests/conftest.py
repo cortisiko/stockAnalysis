7e5fceacb8aa7275359a33adc17575f4ccf49635
cortisiko <curtis.david7@gmail.com>
2020-11-21 18:49:34 -0500

    fixed test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 from app.helpers import tickers as ticker
 
+"""
+learn this tutorial on python pytests. being productive 
+https://www.youtube.com/watch?v=ixqeebhUa-w
+"""
+
 
 @pytest.fixture
 def ticker_symbol():
@@ -8,5 +13,5 @@ def ticker_symbol():
     ticker_symbol_object = ticker.get_ticker(
         ticker_symbol)
     assert ticker_symbol_object is not None
-    
+
     return ticker_symbol_object
--- tests/test_analyze_data.py
+++ tests/test_analyze_data.py
@@ -4,11 +4,9 @@ from app.financials.analyze import get_company_sector, get_current_stock_price
 
 def test_get_company_sector():
     conpany_sector = get_company_sector("C")
-    #print("HOOOOO", conpany_sector)
-    assert get_company_sector("C") is not None
+    assert conpany_sector is not None
 
 
 def test_current_stock_price():
     conpany_price = get_current_stock_price("C")
-    #print("Price", conpany_price)
-    assert get_company_sector("C") is not None
+    assert conpany_price > 0
