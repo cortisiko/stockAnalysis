e9785a2d04aba0c6243c7d8745a51d0b383272a5
cortisiko <curtis.david7@gmail.com>
2020-11-21 18:59:22 -0500

    adding classes to tests

--- tests/test_balance_sheet_data.py
+++ tests/test_balance_sheet_data.py
@@ -1,12 +1,13 @@
 import app.financials.balancesheet as _balance_sheet
 
 
-def test_balance_sheet_data_returned(ticker_symbol):
-    balance_sheet = _balance_sheet.get_balance_sheet_data(ticker_symbol, "a")
-    assert len(balance_sheet) > 0, "The rows are missing in the balance sheet"
-    assert len(balance_sheet.columns) > 0, "The balance sheet is not returning data"
+class TestBalanceSheet:
 
+    def test_balance_sheet_data_returned(self, ticker_symbol):
+        balance_sheet = _balance_sheet.get_balance_sheet_data(ticker_symbol, "a")
+        assert len(balance_sheet) > 0, "The rows are missing in the balance sheet"
+        assert len(balance_sheet.columns) > 0, "The balance sheet is not returning data"
 
-def test_long_term_debt(ticker_symbol):
-    balance_sheet = _balance_sheet.get_balance_sheet_data(ticker_symbol, "a")
-    assert 'LongTermDebt' in balance_sheet
+    def test_long_term_debt(self, ticker_symbol):
+        balance_sheet = _balance_sheet.get_balance_sheet_data(ticker_symbol, "a")
+        assert 'LongTermDebt' in balance_sheet
