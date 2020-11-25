e1c89503ad9084d45acbb97406353b5662a6a7ac
cortisiko <curtis.david7@gmail.com>
2020-11-25 18:17:22 -0500

    cash flow done :moneybag:

--- tests/test_cash_flow.py
+++ tests/test_cash_flow.py
@@ -1,20 +1,21 @@
-import pytest
-from app.helpers import tickers
 from app.financials import cashflowsheet
-
-import logging
 
 """
 https://stackoverflow.com/questions/34466027/in-pytest-what-is-the-use-of-conftest-py-files
 """
 
 
-def test_cash_flow_data_frame(ticker_object):
-    cash_flow_data = cashflowsheet.get_cash_flow_data(ticker_object, "a")
-    assert len(cash_flow_data) > 0, "there is not cash flow data"
+class TestCashFlow:
 
+    def test_cash_flow_data_frame(self, ticker_object):
+        cash_flow_data = cashflowsheet.get_cash_flow_data(ticker_object, "a")
+        assert len(cash_flow_data) > 0, "there is not cash flow data"
 
-def test_free_cash_flow(ticker_object):
-    yearly_free_cash_flow = cashflowsheet.get_cash_flow_data(ticker_object, "a")
+    def test_free_cash_flow(self, ticker_object):
+        yearly_free_cash_flow = cashflowsheet.get_cash_flow_data(ticker_object, "a")
 
-    assert 'FreeCashFlow' in yearly_free_cash_flow, "I do not see the free cash flow column"
+        assert 'FreeCashFlow' in yearly_free_cash_flow, "I do not see the free cash flow data"
+
+    def test_operating_cash_flow(self, ticker_object):
+        quarterly_operating_cash_flow = cashflowsheet.get_operating_cash_flow(ticker_object)
+        assert 'OperatingCashFlow' in quarterly_operating_cash_flow, "I do not see the operating cash flow data"
