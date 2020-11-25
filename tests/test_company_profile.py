52752c342355742a3cdbc33ef338521197ee8b5c
cortisiko <curtis.david7@gmail.com>
2020-11-25 11:02:34 -0500

    added company data tests

--- tests/test_company_profile.py
+++ tests/test_company_profile.py
@@ -3,7 +3,16 @@ import app.financials.companyprofile as company_profile
 
 class TestCompanyProfile:
 
+    def test_company_profile_data_returned(self,ticker_object):
+        company_profile_data = company_profile.get_company_profile(ticker_object)
+        assert company_profile_data is not None, "Nothing is returned for the company profile data"
+
     def test_get_company_sector(self, ticker_object):
         conpany_profile = company_profile.get_company_profile(ticker_object)
-        conpany_sector = conpany_profile["BA"]
-        assert "sector" in conpany_sector
+        conpany_sector = conpany_profile['BA']
+        assert "sector" in conpany_sector, "'Sector' is missing from the company profile data object"
+
+    def test_company_details(self,ticker_object):
+        conpany_profile = company_profile.get_company_profile(ticker_object)
+        company_details = conpany_profile['BA']
+        assert "sector" in company_details, "'longBusinessSummary' is missing from the company profile data object"
