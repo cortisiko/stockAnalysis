7cdca1b536a27e5833124aa8bc582ac79461d02a
cortisiko <curtis.david7@gmail.com>
2020-11-25 19:22:55 -0500

    taking ticker symbol from user

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,28 @@ from app.helpers import tickers as ticker
 """
 learn this tutorial on python pytests. being productive 
 https://www.youtube.com/watch?v=ixqeebhUa-w
+
+and taking args:
+https://stackoverflow.com/questions/44441929/how-to-share-global-variables-between-tests
 """
 
-stock_symbol = "C"
+global ticker_symbol
+
+
+def pytest_addoption(parser):
+    parser.addoption("--ticker", action="store", default="v25", help="By default: v25")
+
+
 @pytest.fixture
-def ticker_object():
-    ticker_symbol = stock_symbol
+def ticker_object(pytestconfig):
+    global ticker_symbol
+
+    ticker_symbol = pytestconfig.getoption("--ticker")
     ticker_symbol_object = ticker.get_ticker(
         ticker_symbol)
     assert ticker_symbol_object is not None
-
     return ticker_symbol_object
+
+@pytest.fixture
+def user_ticker():
+    return ticker_symbol
--- tests/test_company_profile.py
+++ tests/test_company_profile.py
@@ -1,6 +1,4 @@
 import app.financials.companyprofile as company_profile
-from tests.conftest import stock_symbol
-
 
 
 class TestCompanyProfile:
@@ -9,12 +7,12 @@ class TestCompanyProfile:
         company_profile_data = company_profile.get_company_profile(ticker_object)
         assert company_profile_data is not None, "Nothing is returned for the company profile data"
 
-    def test_get_company_sector(self, ticker_object):
+    def test_get_company_sector(self, ticker_object,user_ticker):
         conpany_profile = company_profile.get_company_profile(ticker_object)
-        conpany_sector = conpany_profile[stock_symbol]
+        conpany_sector = conpany_profile[user_ticker]
         assert "sector" in conpany_sector, "'Sector' is missing from the company profile data object"
 
-    def test_company_details(self,ticker_object):
+    def test_company_details(self,ticker_object,user_ticker):
         conpany_profile = company_profile.get_company_profile(ticker_object)
-        company_details = conpany_profile[stock_symbol]
+        company_details = conpany_profile[user_ticker]
         assert "sector" in company_details, "'longBusinessSummary' is missing from the company profile data object"
