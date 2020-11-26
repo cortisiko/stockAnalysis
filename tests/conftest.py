2cc3ec7cc0d432d78a9977fb22c40e821b2601e5
cortisiko <curtis.david7@gmail.com>
2020-11-26 18:59:26 -0500

    testconfig update

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,18 +9,24 @@ and taking args:
 https://stackoverflow.com/questions/44441929/how-to-share-global-variables-between-tests
 """
 
+
+"""
+notes:
+to profile which tests run the slowest use the --durations=3 flag
+ 
+"""
 global ticker_symbol
 
 
 def pytest_addoption(parser):
-    parser.addoption("--ticker", action="store", default="v25", help="By default: v25")
+    parser.addoption("--ticker", action="store", default="BA", help="By default: BA")
 
 
 @pytest.fixture
 def ticker_object(pytestconfig):
     global ticker_symbol
 
-    ticker_symbol = pytestconfig.getoption("--ticker")
+    ticker_symbol = pytestconfig.getoption("--ticker").upper()
     ticker_symbol_object = ticker.get_ticker(
         ticker_symbol)
     assert ticker_symbol_object is not None
