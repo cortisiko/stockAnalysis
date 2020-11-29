f95b5f1adada1aef3d53440fe86f34e98827fa9a
cortisiko <curtis.david7@gmail.com>
2020-11-28 19:08:54 -0500

    statistical page tests

--- tests/test_income_statement.py
+++ tests/test_income_statement.py
@@ -1,4 +1,3 @@
-import pytest
 import app.financials.incomestatementsheet as income_statement
 
 
@@ -7,9 +6,9 @@ class TestIncomeStatement:
     def test_income_statement_data_returned(self, user_ticker, ticker_object):
         income_data = income_statement.get_income_statement(ticker_object, "a")
 
-        assert income_data is not None
+        assert income_data is not None,"income data has nothing returned"
 
     def test_net_income_data_returned(self, user_ticker, ticker_object):
         income_data = income_statement.get_income_statement(ticker_object, "a")
 
-        assert 'NetIncome' in income_data
+        assert 'NetIncome' in income_data,"'NetIncome' is not in the incomesheet dataframe"
